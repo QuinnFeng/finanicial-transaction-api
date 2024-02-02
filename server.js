633caaf1dfd57ad9484479a662f9020bd516468d
QuinnFeng <quinnfeng26276@gmail.com>
2024-02-02 10:25:55 -0500

    added CORS

--- server.js
+++ server.js
@@ -2,21 +2,28 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
 // Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
 
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // Replace '*' with your actual origin
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// server.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*"); // Replace '*' with your actual origin
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 server.use(middlewares);
 // Add this before server.use(router)
+server.use(cors(corsOptions));
 server.use(
   // Add custom route here if needed
   jsonServer.rewriter({

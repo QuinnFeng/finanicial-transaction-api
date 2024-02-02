e934e00efc78c92a58ffa23e94e22e189f2ca188
QuinnFeng <quinnfeng26276@gmail.com>
2024-02-02 11:17:45 -0500

    changed cors option

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 // JSON Server module
 import { create, router as _router, defaults, rewriter } from "json-server";
 import cors from "cors";
+
 const server = create();
 const router = _router("db.json");
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://banking-react-indol.vercel.app/"], // use your actual domain name (or localhost), using * is not recommended
+  origin: ["http://localhost:5173", "https://banking-react-indol.vercel.app/"],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
   allowedHeaders: [
     "Content-Type",
@@ -20,31 +21,24 @@ const corsOptions = {
   credentials: true,
 };
 
-// Make sure to use the default middleware
-const middlewares = defaults();
-
-// server.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*"); // Replace '*' with your actual origin
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
-
-server.use(middlewares);
-// Add this before server.use(router)
+// Middleware setup
 server.use(cors(corsOptions));
+server.use(defaults()); // Make sure to use defaults() after cors() to include default JSON Server middlewares
+
+// Custom route rewriter
 server.use(
-  // Add custom route here if needed
   rewriter({
     "/*": "/$1",
   })
 );
+
+// Router
 server.use(router);
+
 // Listen to port
-server.listen(3000, () => {
-  console.log("JSON Server is running");
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
 });
 
 // Export the Server API

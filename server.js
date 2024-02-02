c23e78dfabc63ef3b97d83476bc5d555fa641f6f
QuinnFeng <quinnfeng26276@gmail.com>
2024-02-02 10:18:22 -0500

    allowed cors

--- server.js
+++ server.js
@@ -6,18 +6,27 @@ const router = jsonServer.router("db.json");
 // Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
 
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Replace '*' with your actual origin
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 server.use(middlewares);
 // Add this before server.use(router)
 server.use(
- // Add custom route here if needed
- jsonServer.rewriter({
-  "/*": "/$1",
- }) 
+  // Add custom route here if needed
+  jsonServer.rewriter({
+    "/*": "/$1",
+  })
 );
 server.use(router);
 // Listen to port
 server.listen(3000, () => {
- console.log("JSON Server is running");
+  console.log("JSON Server is running");
 });
 
 // Export the Server API

5b4061f867e8c64bda91cf99ac9aedf998a90759
QuinnFeng <quinnfeng26276@gmail.com>
2024-02-02 12:00:38 -0500

    fix preflight problem

--- server.js
+++ server.js
@@ -1,30 +1,42 @@
 // JSON Server module
-// JSON Server module
 import { create, router as _router, defaults, rewriter } from "json-server";
+import cors from "cors";
+
 const server = create();
 const router = _router("db.json");
 
-// Make sure to use the default middleware
-const middlewares = defaults();
+const corsOptions = {
+  origin: ["http://localhost:5173", "https://banking-react-indol.vercel.app/"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
+  allowedHeaders: [
+    "Content-Type",
+    "Origin",
+    "X-Requested-With",
+    "Accept",
+    "x-client-key",
+    "x-client-token",
+    "x-client-secret",
+    "Authorization",
+  ],
+  credentials: true,
+};
 
-// Enable CORS by adding the appropriate headers
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // Replace '*' with your actual origin
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
-server.use(middlewares);
-
-// Add this before server.use(router)
+// Middleware setup
+server.use(cors(corsOptions));
+server.use(defaults());
+server.options("/*", cors(corsOptions)); // Handle OPTIONS requests explicitly
 server.use(router);
 
+// Error handling middleware
+server.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
+
 // Listen to port
-server.listen(3000, () => {
-  console.log("JSON Server is running");
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
 });
 
 // Export the Server API
